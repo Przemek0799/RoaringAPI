3bd8a086dae140dadab6b037127b8a497457f0d6
Przemek0799 <przemyslaw.walkowski@student.hv.se>
2023-12-08 10:28:22 +0100

    separata klasser för companyhierarchy

---
+++ Controllers/CompanyHierarchyController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoaringAPI.Model;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+//brings most of the data to dashboard
+namespace RoaringAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompanyHierarchyController : ControllerBase
+    {
+        private readonly RoaringDbcontext _context;
+        private readonly ILogger<CompanyHierarchyController> _logger;
+
+        public CompanyHierarchyController(RoaringDbcontext context, ILogger<CompanyHierarchyController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet("{roaringCompanyId}")]
+        public async Task<ActionResult<CompanyHierarchyRelatedData>> GetCompanyData(string roaringCompanyId)
+        {
+            _logger.LogInformation($"Received request for company data with RoaringCompanyId: {roaringCompanyId}");
+
+            try
+            {
+                var lowerCaseId = roaringCompanyId.ToLower();
+                var companies = await _context.Companies
+                                              .Where(c => c.RoaringCompanyId.ToLower() == lowerCaseId)
+                                              .ToListAsync();
+
+                _logger.LogInformation($"Found {companies.Count} companies matching RoaringCompanyId: {roaringCompanyId}");
+
+                var companyIds = companies.Select(c => c.CompanyId).ToList();
+
+                var addresses = await _context.Addresses
+                                              .Where(a => companyIds.Contains(a.CompanyId))
+                                              .ToListAsync();
+                var employees = await _context.CompanyEmployees
+                                              .Where(e => companyIds.Contains(e.CompanyId))
+                                              .ToListAsync();
+                var financialRecords = await _context.FinancialRecords
+                                               .Where(fr => companyIds.Contains(fr.CompanyId))
+                                                .ToListAsync();
+                var companyRatingIds = companies
+                 .Where(c => c.CompanyRatingId.HasValue)
+                 .Select(c => c.CompanyRatingId.Value)
+                 .Distinct()
+                 .ToList();
+
+                var companyRatings = await _context.CompanyRatings
+                    .Where(cr => companyRatingIds.Contains(cr.CompanyRatingId))
+                    .ToListAsync();
+                var companyStructures = await _context.CompanyStructures
+                                                      .Where(s => companyIds.Contains(s.CompanyId))
+                                                      .ToListAsync();
+
+                _logger.LogInformation($"Found {addresses.Count} addresses, {employees.Count} employees, and {financialRecords.Count},  financial records related to the company.");
+
+                return new CompanyHierarchyRelatedData
+                {
+                    Companies = companies,
+                    Addresses = addresses,
+                    CompanyEmployees = employees,
+                    FinancialRecords = financialRecords,
+                    CompanyRatings = companyRatings,
+                    CompanyStructures = companyStructures,
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error occurred while fetching data for RoaringCompanyId: {roaringCompanyId}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+
+    public class CompanyHierarchyRelatedData
+    {
+        public IEnumerable<Company> Companies { get; set; }
+        public IEnumerable<Address> Addresses { get; set; }
+        public IEnumerable<CompanyEmployee> CompanyEmployees { get; set; }
+        public IEnumerable<FinancialRecord> FinancialRecords { get; set; }
+        public IEnumerable<CompanyRating> CompanyRatings { get; set; }
+        public IEnumerable<CompanyStructure> CompanyStructures { get; set; }
+
+
+    }
+}
