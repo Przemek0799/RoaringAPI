b753ffa1bdcaff4dd2ce479fc1b95aafe32b410b
Przemek0799 <przemyslaw.walkowski@student.hv.se>
2023-12-18 09:21:41 +0100

    logger everywhere instead of writeline

--- Service/ExceptionHandlingService .cs
+++ Service/ExceptionHandlingService .cs
@@ -1,15 +1,23 @@
 ﻿using RoaringAPI.Interface;
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace RoaringAPI.Service
 {
     public class ExceptionHandlingService : IExceptionHandlingService
     {
+        private readonly ILogger<ExceptionHandlingService> _logger;
+
+        public ExceptionHandlingService(ILogger<ExceptionHandlingService> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task HandleExceptionAsync(Exception exception, string customMessage)
         {
-            // Log the exception or perform any other centralized exception handling here
-            Console.WriteLine($"Exception: {customMessage}. Details: {exception.Message}");
+            // Log the exception using the injected logger
+            _logger.LogError(exception, "Exception: {CustomMessage}", customMessage);
         }
     }
 }
